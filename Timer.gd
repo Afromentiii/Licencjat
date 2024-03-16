c43aa3732ec761d25d8605fb67d951b1ef537c61
Jakub Krać <jakubkrac0123@gmail.com>
2024-03-16 19:17:24 +0100

    TESTING

--- Timer.gd
+++ Timer.gd
@@ -1,17 +1,20 @@
 extends Timer
 
 @export var duration: float = 0.25
-var moves = ["right", "jump", "left", "nothing"]
+var positionBeforeMove
+var player : CharacterBody2D
+var moves = ["right", "jump", "nothing"]
 var move = 0
 
 func _ready():
 	self.wait_time = duration
 	self.autostart = true
-	if move == 3:
+	if move == 2:
 		pass
 	else:
 		Input.action_press(moves[move])
 
 func _on_timeout():
 	Input.action_release(moves[move])
+	player.validation(player.feedback(positionBeforeMove), move)
 	queue_free()
