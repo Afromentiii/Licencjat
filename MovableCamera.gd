7f5df6f5cab7c17380b3f6d6e68289f70b4f2246
Jakub Krać <jakubkrac0123@gmail.com>
2024-03-20 23:32:01 +0100

    ADJUSTING CAMERA

--- MovableCamera.gd
+++ MovableCamera.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
-var newPosition = Vector2(0,0)
+var dragging = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,11 +10,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		newPosition = get_local_mouse_position()
-		self.position = newPosition
-		
-	elif  Input.is_action_just_released("S_UP"):
-		self.zoom.x *=2 
-		
+	if Input.is_action_pressed("B_LEFT"):
+		dragging = true
+	else:
+		dragging = false
+	if Input.is_action_pressed("B_LEFT") and dragging:
+		position = get_global_mouse_position()
 
+	if  Input.is_action_just_pressed("S_UP"):
+		if zoom.x + 1 > 6 and zoom.y + 1 > 6:
+			self.zoom.x = 6
+			self.zoom.y = 6
+		else:
+			self.zoom.x += 1
+			self.zoom.y += 1
+		if Input.is_action_just_released("S_UP"):
+			position = get_global_mouse_position()
+	elif Input.is_action_just_pressed("S_DOWN"):
+		if zoom.x - 1 <= 0 and zoom.y - 1 <= 0:
+			zoom.x = 1
+			zoom.y = 1
+		else:
+			self.zoom.x -= 1
+			self.zoom.y -= 1
