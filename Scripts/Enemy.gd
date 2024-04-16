ff07a639d56a4723b7daecf35467ec90be09ed01
Jakub Krać <jakubkrac0123@gmail.com>
2024-04-16 15:09:32 +0200

    Added Enemy Animation, Added correct cleanup when exited

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 50.0
+const SPEED = 25.0
 const JUMP_VELOCITY = -400.0
 var is_population_dead = true
 
@@ -18,19 +18,25 @@ func changeDirection():
 	self.directionL = !self.directionL
 
 func _physics_process(delta):
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
 	is_population_dead = get_parent().get_node("GeneticAlgorithm").is_generation_dead
 	
 	if is_population_dead == false:
+		$AnimatedSprite2D.play("move")
 		if directionL:
+			$AnimatedSprite2D.flip_h = true
 			velocity.x = -1 * SPEED
 		else:
+			$AnimatedSprite2D.flip_h = false
 			velocity.x = 1 * SPEED
 	else: 
 		position = spawnPosition
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		directionL = true
+		$AnimatedSprite2D.play("default")
+		
 	move_and_slide()
 	
