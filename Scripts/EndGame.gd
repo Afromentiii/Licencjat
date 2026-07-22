abc5745291e70774d2282d58069eb2949fafc5f2
Jakub Krać <jakubkrac0123@gmail.com>
2026-07-23 01:04:27 +0200

    feat: nagrody algorytmu i logika życiowa
    feat(core): stworzenie funkcji przyznawania punktów za postępy w postaci kar i nagród
    feat(core): wdrożenie kompleksowej logiki cyklu życia agenta zarządzającej stanem (live function)
    fix(core): konfiguracja niezbędnych timerów synchronizujących ruchy poszczególnych agentów
    feat(environment): utworzenie strefy zakończenia poziomu zatrzymującej czas gry (end_game_area)

--- Scripts/EndGame.gd
+++ Scripts/EndGame.gd
@@ -2,6 +2,5 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	print("XD")
 	if body.has_method("respawn"):
 		body.respawn()
--- Scripts/QLearning.gd
+++ Scripts/QLearning.gd
@@ -2,33 +2,31 @@ extends Node2D
 
 
 var iter = 0
-var states = []
 var call = Callable(self, "learn")
+
 var players = []
+var player_is_dead_counter = 0
+var move : int = 0
+var is_generation_dead : bool = false
+var totalTimeMoves : float = 0.0
+@export var path_to_conf = "res://Levels/Level0/GeneticAlgorithm0/conf.txt"
 
 func learn():
-	await get_tree().create_timer(2).timeout
-	while true:
-		for i in players:
-			var move = randi_range(0,1)
-			i.move(move,randf_range(0,0.5))
-		await get_tree().create_timer(0.5).timeout
-
-
-	
+	await get_tree().create_timer(1).timeout
+	for i in players:
+		i.t.start(i.life, Thread.PRIORITY_HIGH)
 func _ready():
 	for i in range(0,1):
-		var p1 = preload("res://Scenes/player.tscn").instantiate()
-		get_parent().call_deferred("add_child",p1)
-		p1.position = get_parent().spawnPos
-		players.append(p1)
+		var p = preload("res://Scenes/player.tscn").instantiate()
+		get_parent().call_deferred("add_child",p)
+		p.position = get_parent().spawnPos
+		p.respawnPosition = get_parent().spawnPos
+		p.playerID = i
+		p.get_node("Index").text = str(i)
+		players.append(p)
 
 	var t = Thread.new()
 	t.start(call,Thread.PRIORITY_HIGH)
 	
-	
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
