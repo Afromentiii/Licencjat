957442c0e0ed19be1c4569b22464377b1a1e06a1
Jakub Krać <jakubkrac0123@gmail.com>
2026-07-23 01:04:29 +0200

    feat: pierwszy poziom i stabilizacja algorytmu
    feat(level): kompletna budowa układu fizycznego poziomu testowego nr 0 (lvl0)
    feat(environment): dodanie nowej, śmiertelnej przeszkody w formie skryptu czarnej materii
    fix(core): usunięcie błędów w głównej pętli wyliczania zakresów oraz limitów iteracji algorytmu
    fix(core): ustabilizowanie procesu ciągłego zapętlania cykli testowych w uczeniu maszynowym
    test(level): ostateczna weryfikacja i przejście przez agentów najprostszego poziomu

--- Scripts/EndGame.gd
+++ Scripts/EndGame.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.has_method("respawn"):
-		body.respawn()
+		body.endGame()
--- Scripts/QLearning.gd
+++ Scripts/QLearning.gd
@@ -8,7 +8,6 @@ extends Node2D
 @onready var generate_first_gen = $Control/VBoxContainer/GenerateFirstGeneration
 @onready var start_genetic_algorithm = $Control/VBoxContainer/StartGenetic
 
-
 var is_generation_dead : bool = false
 var is_generation_loaded: bool = false
 var is_button_just_pressed : bool = false
@@ -17,6 +16,7 @@ var is_living_process_started : bool = false
 var is_saving : bool = false
 var is_genetic_button_just_pressed : bool = false
 var is_genetic_started : bool = false
+var arrays_are_empty : bool = false
 
 var call = Callable(self, "learn")
 var players = []
@@ -25,10 +25,21 @@ var move : int = 0
 var lines = []
 var gen_population = 0
 var gen_last = 0
+var genetic_iterations = 0
+var genetic_iterations_saved = 0
+var genetic_iter = 0
+var player_moves_is_empty_counter = 0
+
+func overide_conf():
+	var conf = FileAccess.open(path_to_conf, FileAccess.WRITE)
+	conf.store_line("GENERATION_POPULATION " + str(gen_population))
+	conf.store_line("LAST_GENERATION " + str(gen_last))
+	conf.close()
 
 func save_players_data(gen):
 	print("SAVING DATA PROCESS...")
 	var generation = FileAccess.open(path_to_genetic + "gen" + str(gen) + ".txt", FileAccess.WRITE)
+	players.sort_custom(compare_p1_p2_reward)
 	for i in players:
 		print(i.moves,i.reward)
 		var line = str(i.reward) + " "
@@ -79,12 +90,13 @@ func load_generation_procedure():
 				load_players_data(path)
 				await get_tree().create_timer(0.1).timeout
 				for i in players:
+					i.position = i.respawnPosition
 					i.t = Thread.new()
 					i.is_dead = false
-					i.t.start(i.loading, Thread.PRIORITY_HIGH)
+					i.t.start(i.loading, Thread.PRIORITY_HIGH)		
 				is_generation_loaded = true
 			else:
-				print("GENERATION IS NOT DEAD")
+				print("GENERATION IS NOT DEAD!!!")
 		else:
 			print("File does not exist!")
 		is_button_just_pressed = false
@@ -106,7 +118,7 @@ func generate_first_gen_procedure():
 
 func mutate(p1):
 	print(p1.moves)
-	var random_number = randi_range(0,5)
+	var random_number = randi_range(0,100)
 	
 	if random_number >= 0 and random_number <= 5:
 		var random_index = randi_range(0,len(p1.moves) - 1)
@@ -115,18 +127,81 @@ func mutate(p1):
 	print(p1.moves)
 	
 func start_genetic_procedure():
-	if is_genetic_button_just_pressed == true:
-		mutate(players[0])
+	if is_genetic_button_just_pressed == true and is_genetic_started == false:
+		genetic_iterations = int(textArea.text)
+		#print(genetic_iterations)
+		if FileAccess.file_exists(path_to_genetic + "gen" + str(gen_last) + ".txt") :
+			if genetic_iterations > 0:
+				is_population_dead()
+				if is_generation_dead == true:
+					find_the_best_player_and_generate_population()
+					is_genetic_started = true
+					is_generation_dead = false
+					is_saving = true
+					genetic_iterations_saved = genetic_iterations
+					gen_last += 1
+					overide_conf()
+					genetic_iter = 1
+					print("GENETIC PROCESS IS STARTING...", "GENETIC ITERATIONS SAVED: ", genetic_iterations_saved)
+					start_living_process()
+				else:
+					print("GENERATION IS NOT DEAD!!!!")
+			else:
+				print("ITERATION NUMBER CAN T BE NEGATIVE!!!!")
 		is_genetic_button_just_pressed = false
 
 func start_living_process():
-	print("STARTING LIVING PROCESS")
+	print("STARTING LIVING PROCESS...")
 	is_living_process_started = true
 	for i in players:
+		i.position = i.respawnPosition
 		i.t = Thread.new()
 		i.is_dead = false
 		i.t.start(i.life, Thread.PRIORITY_HIGH)
+
+func check_if_moves_are_empty():
+	for i in players:
+		if i.moves.is_empty() == true:
+			player_moves_is_empty_counter += 1
+		else:
+			player_moves_is_empty_counter = 0
+		if player_moves_is_empty_counter == len(players):
+			arrays_are_empty = true
+
+func compare_p1_p2_reward(a, b):
+	if a.reward > b.reward:
+		return true
+	return false
+
+func find_the_best_player_and_generate_population():
+	load_players_data(path_to_genetic + "gen" + str(gen_last) + ".txt")
+	players.sort_custom(compare_p1_p2_reward)
 	
+	var steps = gen_population / 2
+	var index = 0
+	var best_players_move_array = players[0].moves
+	var best_players_move_array2 = players[1].moves
+
+
+	for i in range(0, steps):
+		players[index].moves  = players[i].moves.duplicate()
+		for pop_counter in range(0,randi_range(1,3)):
+			players[index].moves.pop_back()
+		index += 1
+		
+	for i in range(0, steps):
+		players[index].moves  = best_players_move_array.duplicate()
+		for pop_counter in range(0,randi_range(1,3)):
+			players[index].moves.pop_back()
+		index += 1
+
+
+			
+	print("NEW POPULATION IS: ")
+	for i in players:
+		i.reward = 0
+		print(i.moves)
+		
 func learn():
 	await get_tree().create_timer(1).timeout
 
@@ -139,22 +214,41 @@ func learn():
 		if is_generation_dead == true:
 			if is_saving == true:
 				save_players_data(gen_last)
+				await get_tree().create_timer(1).timeout
 				is_saving = false
+			if is_genetic_started == true:
+				print("GENETIC ITER IS ", genetic_iter)
+				get_tree().create_timer(0.1).timeout
+				if genetic_iter < genetic_iterations_saved:
+					is_generation_dead = false
+					find_the_best_player_and_generate_population()
+					await get_tree().create_timer(1).timeout
+					start_living_process()
+					gen_last += 1
+					overide_conf()
+					is_saving = true
+					genetic_iter += 1
+					
+				else:
+					get_tree().create_timer(0.1).timeout
+					genetic_iter = 0
+					is_genetic_started = false
+					genetic_iterations_saved = 0
 		await get_tree().create_timer(0.01).timeout
-	
 
-	'''
-	while true:
-		is_population_dead()
-		if is_generation_dead == true:
-			is_generation_dead = false
-			print("ALL DIED :D")
-			save_players_data()
-			break
-		await get_tree().create_timer(0.1).timeout
-		
-	load_players_data()
-	'''
+
+func set_player_configuration(p, i):
+		p.position = get_parent().spawnPos
+		p.respawnPosition = get_parent().spawnPos
+		p.playerID = i
+		p.get_node("Index").text = str(i)
+		p.is_dead = true
+		var red = randf_range(0,1)
+		var green = randf_range(0,1)
+		var blue = randf_range(0,1)
+		p.modulate = Color(red,green,blue)
+		players.append(p)
+
 func _ready():
 	if FileAccess.file_exists(path_to_conf):
 		var conf = FileAccess.open(path_to_conf, FileAccess.READ)
@@ -170,20 +264,10 @@ func _ready():
 	for i in range(0, gen_population):
 		var p = preload("res://Scenes/player.tscn").instantiate()
 		get_parent().call_deferred("add_child",p)
-		p.position = get_parent().spawnPos
-		p.respawnPosition = get_parent().spawnPos
-		p.playerID = i
-		p.get_node("Index").text = str(i)
-		p.is_dead = true
-		var red = randf_range(0,1)
-		var green = randf_range(0,1)
-		var blue = randf_range(0,1)
-		p.modulate = Color(red,green,blue)
-		players.append(p)
-
+		set_player_configuration(p,i)
 	var t = Thread.new()
 	t.start(call,Thread.PRIORITY_HIGH)
 	
-func _process(delta):
+func _process(_delta):
 	pass
 
